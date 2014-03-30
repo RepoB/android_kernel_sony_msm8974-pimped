ab829bc26d79db9ea85d4b5ca4469005b5d84ebc
Androguide <admin@androguide.fr>
2014-03-30 15:05:57 +0200

    build-script: some more improvements
    Change-Id: I4eedd3b2b6b0be82d68a10d4ac5b2b5542c850ba

--- build-kernel.sh
+++ build-kernel.sh
@@ -21,9 +21,9 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-################################################
-## Example: ./build-kernel.sh honami f2fs -j9 ##
-################################################
+####################################################
+## Example: ./build-kernel.sh --honami --f2fs -j9 ##
+####################################################
 CYAN="\\033[1;36m"
 GREEN="\\033[1;32m"
 YELLOW="\\E[33;44m"
@@ -37,24 +37,26 @@ DATE=`date +%Y%m%d_%H%M`
 # Device selection
 if [ -z "$1" ]
   then
-    echo -n -e "\n${RED}You can specify the device to build for with an argument (e.g: ./build-kernel.sh honami)${RESET}\n\n"
+    echo -n -e "\n${RED}You can specify the device to build for with an argument (e.g: ./build-kernel.sh --honami)${RESET}\n\n"
     echo -n -e "${CYAN}Please input the codename of the device you're building for ${YELLOW}(honami / amami)${RESET}${CYAN} : ${RESET}\n"
     read device
     DEVICE=${device}
 else
     DEVICE=$1
+    DEVICE=${DEVICE:2}
     echo -n -e "${GREEN}Building for ${YELLOW}${DEVICE}${RESET}\n"
 fi
 
 # File-system selection
 if [ -z "$2" ]
   then
-    echo -n -e "\n${RED}You can specify the file-system to use with an argument (e.g: ./build-kernel.sh ext4)${RESET}\n\n"
+    echo -n -e "\n${RED}You can specify the file-system to use with an argument (e.g: ./build-kernel.sh --ext4)${RESET}\n\n"
     echo -n -e "${CYAN}Which file-system to use ? ${YELLOW}(ext4 / f2fs)${RESET}${CYAN} : ${RESET}\n"
     read fs
     FS=${fs} 
 else
     FS=$2
+    FS=${FS:2}
     echo -n -e "${GREEN}Building with the ${YELLOW}${FS}${RESET}${GREEN} file-system...${RESET}\n"
 fi
 
@@ -83,14 +85,14 @@ mka bootimage -j${threads}
 
 # Add boot.img and wlan.ko to the flashable zip
 mkdir -p tmp-dir/system/lib/modules
-cp -f out/target/product/${DEVICE}/boot.img tmp-dir/boot.img
-cp -f out/target/product/${DEVICE}/system/lib/modules/wlan.ko tmp-dir/system/lib/modules/wlan.ko
+cp -f ${OUT_DIR_COMMON_BASE}/${PWD##*/}/target/product/${DEVICE}/boot.img tmp-dir/boot.img
+cp -f ${OUT_DIR_COMMON_BASE}/${PWD##*/}/target/product/${DEVICE}/system/lib/modules/wlan.ko tmp-dir/system/lib/modules/wlan.ko
 cp -f kernel/sony/msm8974/placeholder.zip tmp-dir/placeholder.zip
 cd tmp-dir
 zip -u placeholder.zip boot.img
 zip -u placeholder.zip system/lib/modules/wlan.ko
-mv -f placeholder.zip ../kernel/sony/msm8974/Pimped-Kernel-${DEVICE}-${FS}-${DATE}.zip
 cd ../
+mv -f tmp-dir/placeholder.zip ${OUT_DIR_COMMON_BASE}/${PWD##*/}/target/product/${DEVICE}/Pimped-Kernel-${DEVICE}-${FS}-${DATE}.zip
 
 # Clean-up
 rm -rf tmp-dir
